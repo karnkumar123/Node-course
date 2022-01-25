11bdea2d9548b987d9276be1a69477f0cdf61dac
karnkumar123 <karnkumar.git@gmail.com>
2022-01-25 14:41:13 +0530

    database setup

--- Example/express/server.js
+++ Example/express/server.js
@@ -1,8 +1,28 @@
 const path = require('path');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({path: path.join(__dirname, '../../config.env')});
 
+const DB = process.env.DATABASE_CLOUD.replace('<DATABASE_CLOUD_USERNAME>', process.env.DATABASE_CLOUD_USERNAME)
+                                     .replace('<DATABASE_CLOUD_PASSWORD>', process.env.DATABASE_CLOUD_PASSWORD)
+                                     .replace('<DATABASE_CLOUD_HOSTNAME>', process.env.DATABASE_CLOUD_HOSTNAME)
+                                     .replace('<DATABASE_CLOUD_DBNAME>', process.env.DATABASE_CLOUD_DBNAME)
+
+
+            
+
+
+
 const app = require('./app');
+
+mongoose.connect(DB, {
+    useNewUrlParser:true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}, () => {
+    console.log('Connection to DB successfully !');
+})
 
 
 const PORT = process.env.PORT || 3000;
